64476ad0523c5f421dceaab0e418cf38c2095254
artik777 <arturpojarkov91@gmail.com>
2023-02-05 13:35:45 +0200

    base-button: added unit tests

--- src/components/BaseUI/BaseButton.spec.js
+++ src/components/BaseUI/BaseButton.spec.js
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import { shallowMount } from "@vue/test-utils";
 import BaseButton from "./BaseButton.vue";
 describe("BaseButton", () => {
@@ -15,11 +15,65 @@ describe("BaseButton", () => {
   it("check if BaseButton.vue component exists", () => {
     expect(wrapper.findComponent(BaseButton).exists()).toBe(true);
   });
-  it("get button", () => {
-    wrapper.get("button");
+  it("has root element 'button'", () => {
+    expect(wrapper.element.tagName).toBe("BUTTON");
+  });
+  it("has attribute type='button'", () => {
+    expect(wrapper.attributes("type")).toBe("button");
+  });
+  it("does not have attribute 'disabled' by default", () => {
+    expect(wrapper.attributes("disabled")).toBeUndefined();
+  });
+  it("has attribute 'disabled' when props 'disabled' is set", () => {
+    createWrapper({ props: { disabled: true } });
+    expect(wrapper.attributes("disabled")).toBeDefined();
+  });
+  it("has 'cursor-not-allowed opacity-50' classes when props 'disabled' is set", () => {
+    createWrapper({ props: { disabled: true } });
+    expect(wrapper.classes()).toContain("cursor-not-allowed");
+    expect(wrapper.classes()).toContain("opacity-50");
+  });
+  it("has 'opacity-100' by default", () => {
+    expect(wrapper.classes()).toContain("opacity-100");
   });
   it("render slot", () => {
     createWrapper({ slots: { default: "Create" } });
-    expect(wrapper.html()).toContain("Create");
+    expect(wrapper.text()).toContain("Create");
+  });
+  it("should emit 'click' event when clicked", async () => {
+    let event;
+    const spy = vi.fn((e) => {
+      event = e;
+    });
+    createWrapper({
+      props: { onClick: spy },
+      slots: {
+        default: "Create",
+      },
+    });
+    expect(spy).not.toHaveBeenCalled();
+    const btn = wrapper.find("button");
+    expect(btn).toBeDefined();
+    await btn.trigger("click");
+    expect(spy).toHaveBeenCalled();
+    expect(spy.mock.calls.length).toBe(1);
+    expect(event).toBeInstanceOf(MouseEvent);
+  });
+  it("should not emit 'click' event when disabled and clicked", async () => {
+    const spy = vi.fn();
+    createWrapper({
+      props: {
+        disabled: true,
+        onClick: spy,
+      },
+      slots: {
+        default: "Create",
+      },
+    });
+    expect(spy).not.toHaveBeenCalled();
+    const btn = wrapper.find("button");
+    expect(btn).toBeDefined();
+    await btn.trigger("click");
+    expect(spy).not.toHaveBeenCalled();
   });
 });
--- src/components/BaseUI/BaseButton.vue
+++ src/components/BaseUI/BaseButton.vue
@@ -1,23 +1,31 @@
 <template>
   <button
+    type="button"
     class="rounded-md bg-blue px-3 text-sm font-medium tracking-wider text-white hover:opacity-50 focus:outline-none"
     :class="[
       $attrs.class,
       disabled ? 'cursor-not-allowed opacity-50' : 'opacity-100',
     ]"
     :disabled="disabled"
-    @click="emit('click')"
+    @click="onClick"
   >
     <slot />
   </button>
 </template>
 
 <script setup lang="ts">
-defineProps({
-  disabled: {
-    type: Boolean,
-    required: false,
-  },
-});
+const props = withDefaults(
+  defineProps<{
+    disabled: boolean;
+  }>(),
+  { disabled: false }
+);
 const emit = defineEmits(["click"]);
+const onClick = (e: MouseEvent) => {
+  if (props.disabled) {
+    e.stopPropagation();
+    return;
+  }
+  emit("click", e);
+};
 </script>
