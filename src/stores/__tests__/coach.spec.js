9b1bac9cfdc04f3c9f2795397b197075ebe5737d
artik777 <arturpojarkov91@gmail.com>
2023-02-07 23:09:56 +0200

    added delete coach and ordering coach unit tests

--- src/stores/__tests__/coach.spec.js
+++ src/stores/__tests__/coach.spec.js
@@ -9,7 +9,7 @@ describe("The coach store", () => {
     setActivePinia(createPinia());
   });
 
-  it("check store createCoach action", async () => {
+  it("create new coach", async () => {
     const fullName = ref("Artur Pojarkov");
     const email = ref("artur.pojarkov@example.com");
     const coachName = ref("Penelope Randi");
@@ -20,6 +20,59 @@ describe("The coach store", () => {
       coachName: coachName.value,
     });
     expect(store.coachNodeList[0].child[0].coach).toStrictEqual({
+      fullName: fullName.value,
+      email: email.value,
+    });
+  });
+  it("delete new created coach", async () => {
+    const fullName = ref("Artur Pojarkov");
+    const email = ref("artur.pojarkov@example.com");
+    const coachName = ref("Penelope Randi");
+    const store = useCoachStore();
+    await store.createCoach({
+      fullName: fullName.value,
+      email: email.value,
+      coachName: coachName.value,
+    });
+    expect(store.coachNodeList[0].child[0].coach).toStrictEqual({
+      fullName: fullName.value,
+      email: email.value,
+    });
+    await store.deleteCoach(fullName.value);
+    expect(store.coachNodeList[0].child).toStrictEqual([]);
+  });
+  it("tree node ordering", async () => {
+    const fullName = ref("Artur Pojarkov");
+    const email = ref("artur.pojarkov@example.com");
+    const coachName = ref("Penelope Randi");
+    const store = useCoachStore();
+    await store.createCoach({
+      fullName: fullName.value,
+      email: email.value,
+      coachName: coachName.value,
+    });
+    const fullName2 = ref("Testikus Test");
+    const email2 = ref("testikus.test@example.com");
+    const coachName2 = ref("Penelope Randi");
+    await store.createCoach({
+      fullName: fullName2.value,
+      email: email2.value,
+      coachName: coachName2.value,
+    });
+    expect(store.coachNodeList[0].child[1].coach).toStrictEqual({
+      fullName: fullName2.value,
+      email: email2.value,
+    });
+    await store.changeCoachPosition({
+      fullName: fullName.value,
+      index: 0,
+      goTo: 1,
+    });
+    expect(store.coachNodeList[0].child[0].coach).toStrictEqual({
+      fullName: fullName2.value,
+      email: email2.value,
+    });
+    expect(store.coachNodeList[0].child[1].coach).toStrictEqual({
       fullName: fullName.value,
       email: email.value,
     });
